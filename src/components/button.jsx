95b258e7be5f887d69cc59443d27b0e46821960a
ASUS <pathaknishank007@gmail.com>
2024-09-16 08:25:06 +0530

    file structure added

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,9 +1,10 @@
 import { Button } from "@mui/material";
+import { red } from "@mui/material/colors";
 
 const CustomButton = () => {
   return (
     <>
-      <Button variant="Contained">
+      <Button variant="Contained" sx={{color:'white',background:"red"}}>
         Sign In
       </Button>
     </>
